e426202661102908be19dcece7dcbd9380151f26
Anthony Pardee <apardee@gmail.com>
2025-05-11 15:29:50 -0500

    expose libraries in project definition

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "UniFFISwiftFramework",
+	products: [
+		.library(name: "UniFFISwiftFramework", targets: ["UniFFISwiftFramework"]),
+		.library(name: "uniffi_swift_frameworkFFI", targets: ["uniffi_swift_frameworkFFI"]),
+    ],
     targets: [
 		.target(
 			name: "UniFFISwiftFramework",
